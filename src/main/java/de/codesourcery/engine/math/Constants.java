4727119d6a63e510ea395f321b8b7c73be5d9919
Tobias Gierke <tobias.gierke@code-sourcery.de>
2012-07-17 15:27:02 +0200

    added file

---
+++ src/main/java/de/codesourcery/engine/math/Constants.java
@@ -0,0 +1,11 @@
+package de.codesourcery.engine.math;
+
+public class Constants {
+
+	public static final float PI = 3.1415927f;
+	public static final float PI_HALF = 1.5707963f;
+	public static final float DEG_TO_RAD = 0.008726646f; // 0.5*(PI/180)
+	public static void main(String[] args) {
+		
+	}
+}
